5afa49ec8cc8ae2b3d69b43a3e9e4abb5243d9a4
Jeremy Cavallo <jcbball55@gmail.com>
2026-02-23 21:26:59 -0500

    Clean some stuff up

--- test.ts
+++ test.ts
@@ -1,18 +1,4 @@
-const { remote } = require('webdriverio');
-
-const capabilities = {
-  platformName: 'Android',
-  'appium:automationName': 'UiAutomator2',
-  'appium:deviceName': 'Android',
-  'appium:appPackage': 'com.contractory',
-};
-
-const wdOpts = {
-  hostname: process.env.APPIUM_HOST || 'localhost',
-  port: 4723,
-  logLevel: 'info',
-  capabilities,
-};
+import { remote } from 'webdriverio'
 
 const clickButton = async (driver: any, btnTxt: string) => {
   const btn = await driver.$(`//*[@text="${btnTxt}"]`);
@@ -20,46 +6,33 @@ const clickButton = async (driver: any, btnTxt: string) => {
   await btn.click();
 }
 
-const enterText = async (driver: any, selectorTxt: string, toEnter: string) => {
+const enterText = async (driver: WebdriverIO.Browser, selectorTxt: string, toEnter: string) => {
   const field = await driver.$(`//*[@text="${selectorTxt}"]`);
   await field.waitForDisplayed();
   await field.setValue(toEnter);
 }
 
-async function runTest() {
-  const driver = await remote(wdOpts);
+const run = async () => {
+  const driver = await remote({
+    hostname: 'localhost',
+    port: 4723,
+    logLevel: 'info',
+    capabilities: {
+      platformName: 'Android',
+      'appium:automationName': 'UiAutomator2',
+      'appium:deviceName': 'Android',
+      'appium:appPackage': 'com.contractory',
+    }
+  });
   try {
-    // Press "Sign In"
     await clickButton(driver, "Sign In")
-
-    // Press "Email"
-    await enterText(driver, "Email", process.env.LOGIN_EMAIL)
-
-    // Press "Password"
+    await enterText(driver, "Email", process.env.LOGIN_EMAIL!)
     await enterText(driver, "Password", process.env.LOGIN_PASS!)
-
-    // Press "Sign In"
     await clickButton(driver, "Sign In")
-
-    // Press "Profile"
-    await clickButton(driver, "Profile")
-
-    // Press "Change Name"
-    await clickButton(driver, "Change Name")
-
-    // Enter first name
-    await enterText(driver, "First", "Tyler")
-
-    // Enter last name
-    await enterText(driver, "Last", "Buchanan")
-
-    // Press "Save"
-    await clickButton(driver, "Save")
-
   } finally {
     await driver.pause(100000);
     await driver.deleteSession();
   }
 }
 
-runTest().catch(console.error);
+run().catch(console.error);
