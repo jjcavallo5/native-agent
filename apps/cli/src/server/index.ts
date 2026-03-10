cfeab7aa66330673bed97c07e354721b4f7d9fe9
Jeremy Cavallo <jcbball55@gmail.com>
2026-03-10 13:51:14 -0400

    Change -p to mean platform for server cmd

--- apps/cli/src/server/index.ts
+++ apps/cli/src/server/index.ts
@@ -13,10 +13,10 @@ serverCmd
 
 serverCmd
 	.command('start')
-	.option('-p, --port <port>', 'Port to run on', NATIVE_AGENT_PORT.toString())
+	.option('--port <port>', 'Port to run on', NATIVE_AGENT_PORT.toString())
 	.option('--headless', 'Run headless mode')
 	.option('--device <device id>', 'Device to run on')
-	.option('--platform <platform>', 'Platform to use', 'android')
+	.option('-p, --platform <platform>', 'Platform to use', 'android')
 	.action(start);
 
 serverCmd
