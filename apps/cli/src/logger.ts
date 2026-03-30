e0392d2b34259fe1a5628d79fd8ec6e8729e3628
Jeremy Cavallo <jcbball55@gmail.com>
2026-03-30 13:20:39 -0400

    Add message chaining to logger

--- apps/cli/src/logger.ts
+++ apps/cli/src/logger.ts
@@ -1,30 +1,32 @@
 import {Logger} from 'effect';
 import chalk from 'chalk';
+import {LogLevel} from 'effect';
 
 // Custom logger that outputs log messages to the console
 const logger = Logger.make(({logLevel, message}) => {
 	const logTag = `[${logLevel.label}]: `;
+	const msg = message instanceof Array ? message.join(' ') : message;
 	switch (logLevel.label) {
 		case 'DEBUG':
-			globalThis.console.log(chalk.blue(logTag), message);
+			globalThis.console.log(chalk.blue(logTag), msg);
 			break;
 		case 'INFO':
-			globalThis.console.log(chalk.greenBright(logTag), message);
+			globalThis.console.log(chalk.greenBright(logTag), msg);
 			break;
 		case 'WARN':
-			globalThis.console.log(chalk.yellowBright(logTag), message);
+			globalThis.console.log(chalk.yellowBright(logTag), msg);
 			break;
 		case 'TRACE':
-			globalThis.console.log(chalk.yellowBright(logTag), message);
+			globalThis.console.log(chalk.yellowBright(logTag), msg);
 			break;
 		case 'ERROR':
-			globalThis.console.log(chalk.redBright(logTag), message);
+			globalThis.console.log(chalk.redBright(logTag), msg);
 			break;
 		case 'FATAL':
-			globalThis.console.log(chalk.bgRedBright(logTag), message);
+			globalThis.console.log(chalk.bgRedBright(logTag), msg);
 			break;
 		case 'ALL':
-			globalThis.console.log(logTag, message);
+			globalThis.console.log(logTag, msg);
 			break;
 	}
 });
